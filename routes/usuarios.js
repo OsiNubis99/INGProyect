57d0f88dd6a18a5ecbd3237b70aef4307c908be1
andres <adhurtado.17@est.ucab.edu.ve>
2019-05-18 03:07:11 -0400

    ENT3-002: REST API funcional

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-var {Client} = require('pg');
+var {
+  Client
+} = require('pg');
 var bcrypt = require('bcryptjs');
-
-router.get('/', async function (req, res) {
+//Lista todos los usuarios del sistema
+router.get('/Listar', async function (req, res) {
   var cliente = new Client({
     user: "osdmeqpylslznh",
     password: "451994ddda6585739cb45de567d5901b5ff7c9b5a80e3acc8e5066792af2ecea",
@@ -18,7 +20,7 @@ router.get('/', async function (req, res) {
   res.json(respuesta.rows);
   await cliente.end();
 });
-
+//retorna un objeto, usuario si es todo ok, o {error: "Tipo de error"}
 router.post('/Iniciar', async function (req, res) {
   var cliente = new Client({
     user: "osdmeqpylslznh",
@@ -29,15 +31,32 @@ router.post('/Iniciar', async function (req, res) {
     ssl: true
   });
   await cliente.connect();
-  var respuesta = await cliente.query("SELECT * FROM usuarios WHERE nombreusuario='"+req.body.nombreusuario+"'");
-  if(respuesta.rows.length==1 && bcrypt.compareSync(req.body.clave, respuesta.rows[0].clave)){
-    res.json(respuesta.rows[0]);
-  }else{
-    res.json(false);
+  var respuesta = await cliente.query("SELECT * FROM usuarios WHERE nombreusuario='" + req.body.nombreusuario + "'");
+  if (respuesta.rows.length == 1) {
+    var intentos = respuesta.rows[0].intentos;
+    if (intentos < 3) {
+      if (bcrypt.compareSync(req.body.clave, respuesta.rows[0].clave)) {
+        res.json(respuesta.rows[0]);
+      } else {
+        intentos++;
+        await cliente.query("UPDATE usuarios SET intentos=" + intentos + " WHERE nombreusuario='" + req.body.nombreusuario + "'");
+        res.json({
+          error: "Clave"
+        });
+      }
+    } else {
+      res.json({
+        error: "Bloqueado"
+      })
+    }
+  } else {
+    res.json({
+      error: "Usuario"
+    });
   }
   await cliente.end();
 });
-
+//retorna un bool, true si se puede usar un nombre de usuario
 router.post('/Validar', async function (req, res) {
   var cliente = new Client({
     user: "osdmeqpylslznh",
@@ -48,15 +67,15 @@ router.post('/Validar', async function (req, res) {
     ssl: true
   });
   await cliente.connect();
-  var respuesta = await cliente.query("SELECT * FROM usuarios WHERE nombreusuario='"+req.body.nombreusuario+"'");
-  if(respuesta.rows.length==0){
+  var respuesta = await cliente.query("SELECT * FROM usuarios WHERE nombreusuario='" + req.body.nombreusuario + "'");
+  if (respuesta.rows.length == 0) {
     res.json(true);
-  }else{
+  } else {
     res.json(false);
   }
   await cliente.end();
 });
-
+//retorna el perfil del usuario agregado
 router.post('/Agregar', async function (req, res) {
   var cliente = new Client({
     user: "osdmeqpylslznh",
@@ -68,12 +87,57 @@ router.post('/Agregar', async function (req, res) {
   });
   var clave = bcrypt.hashSync(req.body.clave, 10);
   await cliente.connect();
-  var codigo = await cliente.query("select count(codigoempleado) from usuarios");
-  codigo =  codigo.rowCount + 1;
+  var codigo = await cliente.query("select count(*) from usuarios");
+  codigo = parseInt(codigo.rows[0].count)+1;
+  codigo = codigo+"-000000".replace(/0/g, function () {return (~~(Math.random() * 16)).toString(16);});
+  if(req.body.cargo){
+    codigo = "SUP-"+codigo;
+  }else{
+    codigo = "OPE-"+codigo;
+  }
   var respuesta = await cliente
-  .query("INSERT INTO usuarios (codigoempleado,nombre,apellido,nombreusuario,cargo,clave,clavetemporal,intentos)"
-    +" VALUES ('"+codigo+"', '"+req.body.nombre+"', '"+req.body.apellido+"', '"+req.body.nombreusuario+"', "+req.body.cargo+", '"+clave+"', true, 0);");
+    .query("INSERT INTO usuarios (codigoempleado,nombre,apellido,nombreusuario,cargo,clave,clavetemporal,intentos)" +
+      " VALUES ('" + codigo + "', '" + req.body.nombre + "', '" + req.body.apellido + "', '" + req.body.nombreusuario + "', " + req.body.cargo + ", '" + clave + "', true, 0) RETURNING *;");
   res.json(respuesta.rows);
+  await cliente.end();
+});
+//retorna el nuevo estado del usuario
+router.put('/Editar', async function (req, res) {
+  var cliente = new Client({
+    user: "osdmeqpylslznh",
+    password: "451994ddda6585739cb45de567d5901b5ff7c9b5a80e3acc8e5066792af2ecea",
+    database: "d4u0fuis94bmbi",
+    port: 5432,
+    host: "ec2-54-83-36-37.compute-1.amazonaws.com",
+    ssl: true
+  });
+  var clave = bcrypt.hashSync(req.body.clave, 10);
+  await cliente.connect();
+  var respuesta = await cliente.query("UPDATE usuarios SET nombre='" + req.body.nombre + "', apellido='" + req.body.apellido + "', cargo=" + req.body.cargo + ", clave='"+clave+"', clavetemporal=" + req.body.clavetemporal + ", intentos=" + req.body.intentos + " WHERE codigoempleado='" + req.body.codigoempleado + "' RETURNING *");
+  if (respuesta.rows.length == 1) {
+    res.json(respuesta.rows);
+  } else {
+    res.json(false);
+  }
+  await cliente.end();
+});
+//retorna true si se elimino el usuario
+router.delete('/Eliminar', async function (req, res) {
+  var cliente = new Client({
+    user: "osdmeqpylslznh",
+    password: "451994ddda6585739cb45de567d5901b5ff7c9b5a80e3acc8e5066792af2ecea",
+    database: "d4u0fuis94bmbi",
+    port: 5432,
+    host: "ec2-54-83-36-37.compute-1.amazonaws.com",
+    ssl: true
+  });
+  await cliente.connect();
+  var respuesta = await cliente.query("delete from usuarios where codigoempleado='"+req.body.codigoempleado+"' RETURNING *");
+  if (respuesta.rowCount == 1) {
+    res.json(true);
+  } else {
+    res.json(false);
+  }
   await cliente.end();
 });
 
