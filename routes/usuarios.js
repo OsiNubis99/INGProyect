fc32a5891ae0aff11c8afe3caccfcda3b94f28df
andres <adhurtado.17@est.ucab.edu.ve>
2019-05-18 16:24:38 -0400

    ENT3-001: FIN Backend terminado.

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -5,139 +5,163 @@ var {
   Client
 } = require('pg');
 var bcrypt = require('bcryptjs');
+
+const datosdb = {
+  user: "postgres",
+  password: "root",
+  database: "postgres",
+  port: 5432,
+  host: "localhost",
+  ssl: true
+}
+/*{
+  user: "osdmeqpylslznh",
+  password: "451994ddda6585739cb45de567d5901b5ff7c9b5a80e3acc8e5066792af2ecea",
+  database: "d4u0fuis94bmbi",
+  port: 5432,
+  host: "ec2-54-83-36-37.compute-1.amazonaws.com",
+  ssl: true
+}
+*/
 //Lista todos los usuarios del sistema
-router.get('/Listar', async function (req, res) {
-  var cliente = new Client({
-    user: "osdmeqpylslznh",
-    password: "451994ddda6585739cb45de567d5901b5ff7c9b5a80e3acc8e5066792af2ecea",
-    database: "d4u0fuis94bmbi",
-    port: 5432,
-    host: "ec2-54-83-36-37.compute-1.amazonaws.com",
-    ssl: true
-  });
+router.get('/Listar', async (req, res) => {
+  var cliente = new Client(datosdb);
   await cliente.connect();
-  var respuesta = await cliente.query("SELECT * FROM usuarios");
-  res.json(respuesta.rows);
+  await cliente.query("SELECT * FROM usuarios")
+    .then((datos) => {
+      res.json(datos.rows)
+    }).catch((err) => {
+      res.json({
+        error: "Error del servidor: " + err
+      });
+    });
   await cliente.end();
 });
 //retorna un objeto, usuario si es todo ok, o {error: "Tipo de error"}
-router.post('/Iniciar', async function (req, res) {
-  var cliente = new Client({
-    user: "osdmeqpylslznh",
-    password: "451994ddda6585739cb45de567d5901b5ff7c9b5a80e3acc8e5066792af2ecea",
-    database: "d4u0fuis94bmbi",
-    port: 5432,
-    host: "ec2-54-83-36-37.compute-1.amazonaws.com",
-    ssl: true
-  });
+router.post('/Iniciar', async (req, res) => {
+  var cliente = new Client(datosdb);
   await cliente.connect();
-  var respuesta = await cliente.query("SELECT * FROM usuarios WHERE nombreusuario='" + req.body.nombreusuario + "'");
-  if (respuesta.rows.length == 1) {
-    var intentos = respuesta.rows[0].intentos;
-    if (intentos < 3) {
-      if (bcrypt.compareSync(req.body.clave, respuesta.rows[0].clave)) {
-        res.json(respuesta.rows[0]);
+  await cliente.query("SELECT * FROM usuarios WHERE nombreusuario='" + req.body.nombreusuario + "'")
+    .then(async (respuesta) => {
+      if (respuesta.rowCount == 1) {
+        var intentos = respuesta.rows[0].intentos;
+        if (intentos < 3) {
+          if (bcrypt.compareSync(req.body.clave, respuesta.rows[0].clave)) {
+            res.json(respuesta.rows[0]);
+          } else {
+            intentos++;
+            await cliente.query("UPDATE usuarios SET intentos=" + intentos + " WHERE nombreusuario='" + req.body.nombreusuario + "'")
+              .catch((err) => {
+                res.json({
+                  error: "Error del servidor: " + err
+                });
+              });
+            res.json({
+              error: "Clave o Usuario"
+            });
+          }
+        } else {
+          res.json({
+            error: "Bloqueado"
+          })
+        }
       } else {
-        intentos++;
-        await cliente.query("UPDATE usuarios SET intentos=" + intentos + " WHERE nombreusuario='" + req.body.nombreusuario + "'");
         res.json({
-          error: "Clave"
+          error: "Clave o Usuario"
         });
       }
-    } else {
+    }).catch((err) => {
       res.json({
-        error: "Bloqueado"
-      })
-    }
-  } else {
-    res.json({
-      error: "Usuario"
+        error: "Error del servidor: " + err
+      });
     });
-  }
   await cliente.end();
 });
 //retorna un bool, true si se puede usar un nombre de usuario
-router.post('/Validar', async function (req, res) {
-  var cliente = new Client({
-    user: "osdmeqpylslznh",
-    password: "451994ddda6585739cb45de567d5901b5ff7c9b5a80e3acc8e5066792af2ecea",
-    database: "d4u0fuis94bmbi",
-    port: 5432,
-    host: "ec2-54-83-36-37.compute-1.amazonaws.com",
-    ssl: true
-  });
+router.post('/Validar', async (req, res) => {
+  var cliente = new Client(datosdb);
   await cliente.connect();
-  var respuesta = await cliente.query("SELECT * FROM usuarios WHERE nombreusuario='" + req.body.nombreusuario + "'");
-  if (respuesta.rows.length == 0) {
-    res.json(true);
-  } else {
-    res.json(false);
-  }
+  await cliente.query("SELECT * FROM usuarios WHERE UPPER(nombreusuario) = UPPER('" + req.body.nombreusuario + "')")
+    .then((respuesta) => {
+      if (respuesta.rowCount == 0) {
+        res.json(true);
+      } else {
+        res.json(false);
+      }
+    }).catch((err) => {
+      res.json({
+        error: "Error del servidor: " + err
+      });
+    });
   await cliente.end();
 });
 //retorna el perfil del usuario agregado
-router.post('/Agregar', async function (req, res) {
-  var cliente = new Client({
-    user: "osdmeqpylslznh",
-    password: "451994ddda6585739cb45de567d5901b5ff7c9b5a80e3acc8e5066792af2ecea",
-    database: "d4u0fuis94bmbi",
-    port: 5432,
-    host: "ec2-54-83-36-37.compute-1.amazonaws.com",
-    ssl: true
-  });
-  var clave = bcrypt.hashSync(req.body.clave, 10);
+router.post('/Agregar', async (req, res) => {
+  var cliente = new Client(datosdb);
   await cliente.connect();
-  var codigo = await cliente.query("select count(*) from usuarios");
-  codigo = parseInt(codigo.rows[0].count)+1;
-  codigo = codigo+"-000000".replace(/0/g, function () {return (~~(Math.random() * 16)).toString(16);});
-  if(req.body.cargo){
-    codigo = "SUP-"+codigo;
-  }else{
-    codigo = "OPE-"+codigo;
-  }
-  var respuesta = await cliente
-    .query("INSERT INTO usuarios (codigoempleado,nombre,apellido,nombreusuario,cargo,clave,clavetemporal,intentos)" +
-      " VALUES ('" + codigo + "', '" + req.body.nombre + "', '" + req.body.apellido + "', '" + req.body.nombreusuario + "', " + req.body.cargo + ", '" + clave + "', true, 0) RETURNING *;");
-  res.json(respuesta.rows);
+  var codigo = "";
+  await cliente.query("select count(*) from usuarios")
+    .then((respuesta) => {
+      codigo = parseInt(respuesta.rows[0].count) + 1;
+      codigo = codigo + "-000000".replace(/0/g, () => {
+        return (~~(Math.random() * 16)).toString(16);
+      });
+      if (req.body.cargo) {
+        codigo = "SUP-" + codigo;
+      } else {
+        codigo = "OPE-" + codigo;
+      }
+    }).catch((err) => {
+      res.json({
+        error: "Error del servidor: " + err
+      });
+    });
+  await cliente.query("INSERT INTO usuarios (codigoempleado,nombre,apellido,nombreusuario,cargo,clave,clavetemporal,intentos)" +
+      " VALUES ('" + codigo + "', '" + req.body.nombre + "', '" + req.body.apellido + "', '" + req.body.nombreusuario + "', " +
+      req.body.cargo + ", '" + bcrypt.hashSync(req.body.clave, 10) + "', true, 0) RETURNING *;")
+    .then((respuesta) => {
+      res.json(respuesta.rows);
+    }).catch((err) => {
+      res.json({
+        error: "Error del servidor: " + err
+      });
+    });
   await cliente.end();
 });
 //retorna el nuevo estado del usuario
-router.put('/Editar', async function (req, res) {
-  var cliente = new Client({
-    user: "osdmeqpylslznh",
-    password: "451994ddda6585739cb45de567d5901b5ff7c9b5a80e3acc8e5066792af2ecea",
-    database: "d4u0fuis94bmbi",
-    port: 5432,
-    host: "ec2-54-83-36-37.compute-1.amazonaws.com",
-    ssl: true
-  });
-  var clave = bcrypt.hashSync(req.body.clave, 10);
+router.put('/Editar', async (req, res) => {
+  var cliente = new Client(datosdb);
   await cliente.connect();
-  var respuesta = await cliente.query("UPDATE usuarios SET nombre='" + req.body.nombre + "', apellido='" + req.body.apellido + "', cargo=" + req.body.cargo + ", clave='"+clave+"', clavetemporal=" + req.body.clavetemporal + ", intentos=" + req.body.intentos + " WHERE codigoempleado='" + req.body.codigoempleado + "' RETURNING *");
-  if (respuesta.rows.length == 1) {
-    res.json(respuesta.rows);
-  } else {
-    res.json(false);
-  }
+  await cliente.query("UPDATE usuarios SET nombre='" + req.body.nombre + "', apellido='" + req.body.apellido + "', cargo=" + req.body.cargo + ", clave='" + bcrypt.hashSync(req.body.clave, 10) + "', clavetemporal=" + req.body.clavetemporal + ", intentos=" + req.body.intentos + " WHERE codigoempleado='" + req.body.codigoempleado + "' RETURNING *")
+  .then((respuesta) => {
+    if (respuesta.rowCount == 1) {
+      res.json(respuesta.rows);
+    } else {
+      res.json(false);
+    }
+  }).catch((err) => {
+    res.json({
+      error: "Error del servidor: " + err
+    });
+  });
   await cliente.end();
 });
 //retorna true si se elimino el usuario
-router.delete('/Eliminar', async function (req, res) {
-  var cliente = new Client({
-    user: "osdmeqpylslznh",
-    password: "451994ddda6585739cb45de567d5901b5ff7c9b5a80e3acc8e5066792af2ecea",
-    database: "d4u0fuis94bmbi",
-    port: 5432,
-    host: "ec2-54-83-36-37.compute-1.amazonaws.com",
-    ssl: true
-  });
+router.delete('/Eliminar', async (req, res) => {
+  var cliente = new Client(datosdb);
   await cliente.connect();
-  var respuesta = await cliente.query("delete from usuarios where codigoempleado='"+req.body.codigoempleado+"' RETURNING *");
-  if (respuesta.rowCount == 1) {
-    res.json(true);
-  } else {
-    res.json(false);
-  }
+  await cliente.query("delete from usuarios where codigoempleado='" + req.body.codigoempleado + "' RETURNING *")
+  .then((respuesta) => {
+    if (respuesta.rowCount == 1) {
+      res.json(true);
+    } else {
+      res.json(false);
+    }
+  }).catch((err) => {
+    res.json({
+      error: "Error del servidor: " + err
+    });
+  });
   await cliente.end();
 });
 
