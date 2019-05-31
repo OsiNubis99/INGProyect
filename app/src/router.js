1d11430ac54169c5afc712ff3ce7b07106a12aff
JMTeixeira17 <49705208+JMTeixeira17@users.noreply.github.com>
2019-05-30 23:03:33 -0400

    ENT3-002: Reparaciones
    Redirección automática a ''/iniciar''

--- app/src/router.js
+++ app/src/router.js
@@ -2,20 +2,5 @@ import Vue from 'vue'
 import Router from 'vue-router'
 
 Vue.use(Router)
+// Rutas de direccion de  URL 
 
-export default new Router({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes:[
-    {
-      name:'Iniciar',
-      path:'/Iniciar',
-      component: () => import('./views/Iniciar.vue')
-    },
-    {
-      name:'Home',
-      path:'/',
-      component: () => import('./views/Inicio.vue')
-    }
-  ]
-})
